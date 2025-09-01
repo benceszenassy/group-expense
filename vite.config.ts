f99ccc8d2b59410252a89d56f02d28d9c10f520e
bence.szenassy <bence.szenassy@gmail.com>
2025-09-01 21:28:17 +0200

    fix: do not use base path in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     vue(),
     vueDevTools(),
@@ -84,5 +84,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/group-expense',
-})
+  base: command === 'build' ? '/group-expense' : '',
+}))
